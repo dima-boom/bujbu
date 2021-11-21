a447fe3b1578eb6df407740372e046d8a7a4834d
dima-boom <78297357+dima-boom@users.noreply.github.com>
2021-11-22 00:09:59 +0300

    Update Musor.py

--- Musor.py
+++ Musor.py
@@ -63,9 +63,9 @@ try:
         cur.execute(f"SELECT * FROM tab WHERE id = '{user_id}'")
         bot.send_message(user_id, f"1. J", reply_markup=markup)
         text = str(cur.fetchall()[0][1])
-        token = cur.fetchall()[0][2]
+        token = str(cur.fetchall()[0][2])
         bot.send_message(user_id, f"Глав", reply_markup=markup)
-        vk_session = vk_api.VkApi(token=str(token))
+        vk_session = vk_api.VkApi(token=token)
         vk = vk_session.get_api()
         while True:
             try:
