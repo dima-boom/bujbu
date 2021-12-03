8cf483142404a28e86e8adef0a3310dee778e3aa
dima-boom <78297357+dima-boom@users.noreply.github.com>
2021-12-03 22:39:07 +0300

    Update Musor.py

--- Musor.py
+++ Musor.py
@@ -26,7 +26,7 @@ try:
     def extract_arg2(arg2):
         return arg2.split()[2]
 
-    bot = telebot.TeleBot('5020584672:AAF3rCwCjvOhzanGdDPn6lkM7ZiaKSeKTkI')
+    bot = telebot.TeleBot('5020584672:AAGKDwapVosl-A5MUo-Qoa_wTIFPSHAKCn4')
 
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
